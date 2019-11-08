ee2dc0dc51ea0a986dd96b65ddb3c218296b9f4a
ramons <ramon.santos@me.com.br>
2019-11-08 13:33:47 -0300

    Uploading files to storage account (blob containers)

--- BlobStorageV12/Program.cs
+++ BlobStorageV12/Program.cs
@@ -9,12 +9,27 @@ namespace BlobStorageV12
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Azure Blob Storage V12 - .NET sample");
             string connectionString = Environment.GetEnvironmentVariable("CONNECT_STR");
-            Console.WriteLine(connectionString); //Here you can check if your environment variable is working
-            Console.ReadKey();
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            string containerName = "blobfromapp" + Guid.NewGuid().ToString();
+            BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+
+            string localPath = "../data/";
+            string fileName = "myFile" + Guid.NewGuid().ToString() + ".txt";
+            string localFilePath = Path.Combine(localPath, fileName);
+
+            await File.WriteAllTextAsync(localFilePath, "Hello, Mediums");
+
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+            Console.WriteLine($"Uploading to Blob Storage:\n\t {blobClient.Uri}\n");
+
+            using FileStream uploadFileStream = File.OpenRead(localFilePath);
+            await blobClient.UploadAsync(uploadFileStream);
+            uploadFileStream.Close();
         }
     }
 }

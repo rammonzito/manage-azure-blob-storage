8f572c528d3ae182435123aab805a25a38807099
ramons <ramon.santos@me.com.br>
2019-11-08 12:41:57 -0300

    first step

---
+++ BlobStorageV12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BlobStorageV12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Azure Blob Storage V12 - .NET sample");
+            string connectionString = Environment.GetEnvironmentVariable("CONNECT_STR");
+            Console.WriteLine(connectionString); //Here you can check if your environment variable is working
+            Console.ReadKey();
+        }
+    }
+}
